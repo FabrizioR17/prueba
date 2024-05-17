4a8cc98031ed712bfdcfee7b180684bda36341a7
= <=>
2024-05-16 23:36:03 -0300

    Comiteando cambios

--- src/com/backend/repository/impl/OdontologoDaoEnMemoria.java
+++ src/com/backend/repository/impl/OdontologoDaoEnMemoria.java
@@ -2,11 +2,13 @@ package com.backend.repository.impl;
 
 import com.backend.model.Odontologo;
 import com.backend.repository.IDao;
+import org.apache.log4j.Logger;
 
 import java.util.List;
 
 public class OdontologoDaoEnMemoria implements IDao<Odontologo> {
     private List<Odontologo> odontologoRepositorio;
+    private final Logger LOGGER = Logger.getLogger(OdontologoDaoEnMemoria.class);
 
     public OdontologoDaoEnMemoria(List<Odontologo> odontologoRepositorio){
         this.odontologoRepositorio = odontologoRepositorio;
@@ -14,12 +16,23 @@ public class OdontologoDaoEnMemoria implements IDao<Odontologo> {
 
     @Override
     public Odontologo registrarOdontologo(Odontologo odontologo) {
+
+        try{
+
+        }catch(Exception e){
+
+        }finally{
+
+        };
         odontologoRepositorio.add(odontologo);
+        LOGGER.info("Odontólogo registrado correctamente: " + odontologoRepositorio);
         return odontologo;
     }
 
     @Override
     public List<Odontologo> listarOdontologo() {
+        LOGGER.info("Lista de odontologos registrados actualmente: " + odontologoRepositorio);
+
         return odontologoRepositorio;
     }
 }
