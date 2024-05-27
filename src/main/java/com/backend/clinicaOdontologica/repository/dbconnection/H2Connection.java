19fac2ecbe6ea59a05117bd3c7c7d50b90c938f5
FabrizioR17 <fabriziora12@gmail.com>
2024-05-27 15:10:45 -0500

    prueba

--- src/main/java/com/backend/clinicaOdontologica/repository/dbconnection/H2Connection.java
+++ src/main/java/com/backend/clinicaOdontologica/repository/dbconnection/H2Connection.java
@@ -7,14 +7,14 @@ import java.sql.SQLException;
 public class H2Connection {
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("org.h2.Driver");
-        return DriverManager.getConnection("jdbc:h2:~/Rodriguez-Fabrizio-Gadea-Fabio","sa", "sa");
+        return DriverManager.getConnection("jdbc:h2:~/Prueba1maven","sa", "sa");
     }
 
     public static void ejecutarScriptInicial() {
         Connection connection = null;
         try {
             Class.forName("org.h2.Driver");
-            connection = DriverManager.getConnection("jdbc:h2:~/Rodriguez-Fabrizio-Gadea-Fabio;INIT=RUNSCRIPT FROM 'src/main/create.sql'", "sa", "sa");
+            connection = DriverManager.getConnection("jdbc:h2:~/Prueba1maven;INIT=RUNSCRIPT FROM 'src/main/create.sql'", "sa", "sa");
 
         } catch (Exception e) {
             e.printStackTrace();
